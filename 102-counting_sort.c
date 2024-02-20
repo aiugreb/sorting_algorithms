de3134083a498e6f30591a5521560ed5781c0644
mohamed amine berguia <mohamedamineberguia@gmail.com>
2024-02-20 12:00:22 -0800

    counting

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,14 +1,16 @@
 #include "sort.h"
 
 /**
- * counting_sort - Entry point
+ * counting_sort - sorts an array of integers in ascending order using the
+ * Counting sort algorithm
  * @array: The character to print
  * @size: The character to print
- * Return: -
+ * 
+ * Return: void
  */
 void counting_sort(int *array, size_t size)
 {
-	int *count_array, *new_array, max = 0, i = 0;
+	int *count, *new_array, max = 0, i = 0;
 
 	if (!array || size < 2)
 		return;
@@ -18,19 +20,19 @@ void counting_sort(int *array, size_t size)
 		if (max < array[i])
 			max = array[i];
 	}
-	count_array = malloc(sizeof(int) * max + 1);
+	count = malloc(sizeof(int) * max + 1);
 	for (i = 0; i <= max; i++)
-		count_array[i] = 0;
+		count[i] = 0;
 	for (i = 0; i < (int)size; i++)
-		count_array[array[i]]++;
+		count[array[i]]++;
 	for (i = 1; i <= max; i++)
-		count_array[i] = count_array[i - 1] + count_array[i];
+		count[i] = count[i - 1] + count[i];
 	new_array = malloc(sizeof(int) * size + 1);
-	print_array(count_array, max + 1);
+	print_array(count, max + 1);
 	for (i = 0; i < (int)size; i++)
 	{
-		new_array[count_array[array[i]] - 1] = array[i];
-		count_array[array[i]]--;
+		new_array[count[array[i]] - 1] = array[i];
+		count[array[i]]--;
 	}
 	for (i = 0; i < (int)size; i++)
 		array[i] = new_array[i];
